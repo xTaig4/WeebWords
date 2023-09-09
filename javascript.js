a5832be787650397ee83a53b9a541c5325384521
xTaig4 <xTaig4@gmail.com>
2023-09-09 16:00:08 +0200

    button, header, img
    added function for button event, that changes img, and header text \O/

--- javascript.js
+++ javascript.js
@@ -1,2 +1,15 @@
 
-alert("You entered the realm of Tai's profile page");
+let IsAwake = false;
+
+function OnButtonClick() {
+    if (IsAwake == false){
+        document.getElementById("myHeader").innerText = 'Wake up to reality, nothing ever goes as planned';
+        document.getElementById("myImg").src = "https://i.pinimg.com/564x/84/dd/07/84dd070d4d13eda6523a2731bacfa9fd.jpg";
+        IsAwake = true;
+    }
+    else{
+        document.getElementById("myHeader").innerText = 'Welcome to my profile page';
+        document.getElementById("myImg").src = "https://pbs.twimg.com/media/F4Tgj9KbcAA4bSP?format=jpg&name=small";
+        IsAwake = false;
+    } 
+}
