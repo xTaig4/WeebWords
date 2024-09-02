6f27f04dd9ba145253fcee80f50f585ff4353b67
TaiStudent <xTaig4@gmail.com>
2024-09-02 19:15:20 +0200

    added background, new quotes, header alignment adjusted, youtube embeded video

--- javascript.js
+++ javascript.js
@@ -3,13 +3,15 @@ let IsAwake = false;
 
 function OnButtonClick() {
     if (IsAwake == false){
-        document.getElementById("myHeader").innerText = 'Wake up to reality, nothing ever goes as planned';
-        document.getElementById("myImg").src = "https://i.pinimg.com/564x/84/dd/07/84dd070d4d13eda6523a2731bacfa9fd.jpg";
+        document.getElementById("myHeader").innerText = 'Can i. . .';
+        document.getElementById("myImg").src = "https://i.pinimg.com/736x/05/0c/95/050c95ee6545449b5ca5712a51f917bb.jpg";
+        document.getElementById("quoteDisplay").innerText = 'Hey, Izuku. . . Can i still catch up to you?'
         IsAwake = true;
     }
     else{
-        document.getElementById("myHeader").innerText = 'Welcome to my profile page';
-        document.getElementById("myImg").src = "https://pbs.twimg.com/media/F4Tgj9KbcAA4bSP?format=jpg&name=small";
+        document.getElementById("myHeader").innerText = 'Wake up. . .';
+        document.getElementById("myImg").src = "https://i.pinimg.com/564x/84/dd/07/84dd070d4d13eda6523a2731bacfa9fd.jpg";
+        document.getElementById("quoteDisplay").innerText = 'Wake up to reality, nothing ever goes as planned'
         IsAwake = false;
     } 
 }
