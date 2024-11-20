4c36aa50be3d74d7b4c4d2595d2cad2a75b97c37
TaiStudent <xTaig4@gmail.com>
2024-11-20 14:05:49 +0100

    changes for debuggin

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ root.render(
     }}>
         <div>
             <title>The Honered One</title>
-            <RandomQuoteSection/>
+            <QuoteComponent/>
             <Router>
                 <div>
                     {/* Navigation Links */}
@@ -38,15 +38,15 @@ root.render(
 
 
 
-function RandomQuoteSection() {
+function QuoteComponent() {
     return <section>
         <div className='column-container'>
             <div className='left-column' >
                 <pre id="quote">
-                            Quote text for display <br></br>
+                            Quote text for display  <br></br>
                             I just have to win. . . right, Izuku?
                 </pre>
-                <button className='random-button' style={{ width: '400px', height: '44px' }} onClick={GetRandomQuote}> Random quote! </button>
+                <button className='random-button' onClick={GetRandomQuote}> Random quote! </button>
             </div>
 
             <div className='right-column'>
@@ -64,52 +64,54 @@ function RandomQuoteSection() {
 
 
 async function GetRandomQuote() {
-    try {
-        await fetch('https://localhost:7028/api/Quotes/Random/QuoteDTO')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');  
-                }
-                return response.json(); // Parse the JSON from the response 
-            })
-            .then(data => {
-                const imgElement = document.getElementById("myImg")! as HTMLImageElement;
-                imgElement.src = data.image;
-                document.getElementById("quote")!.innerText = data._Quote;
-            })    
-    } catch (error) {
-        const imgElement = document.getElementById("myImg")! as HTMLImageElement;
-        if (imgElement.src = "https://i.pinimg.com/564x/81/5b/55/815b55146b9ff6555986bad7b9bc00d6.jpg") {
-            imgElement.src = "https://i.pinimg.com/736x/6c/f6/62/6cf662ab5632f2df41e84e5d5144d2d9.jpg"
-            document.getElementById("quoteDisplay")!.innerText = "I just have to win. . . right, Izuku?"
-        }
-        else {
-            imgElement.src = "https://i.pinimg.com/564x/81/5b/55/815b55146b9ff6555986bad7b9bc00d6.jpg"
-            document.getElementById("quoteDisplay")!.innerText = "I'll never go back on my Word. That's my Nindo: my ninja way!!";
-        }
-    }
-}
-
-async function PostQuote(newQuote: { firstName: string, lastName: string, _Quote: string, image: string }) {
-    await fetch('https://localhost:7028/api/Quote', {
-        method: 'POST', // Specify the HTTP method as POST
-        headers: {
-            'Content-Type': 'application/json' // Set the content type to JSON
-        },
-        body: JSON.stringify(newQuote) // Convert the JavaScript object to a JSON string
-    })
+    await fetch('https://localhost:7028/api/Quotes/Random/QuoteDTO')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-
-            return response.json(); // Parse the JSON from the response
+            return response.json(); // Parse the JSON from the response 
         })
         .then(data => {
-            console.log('Quote successfully posted:', data);
-            // You can handle the response data if needed
+            const imgElement = document.getElementById("myImg")! as HTMLImageElement;
+            imgElement.src = data.image;
+            document.getElementById("quote")!.innerText = data._Quote + ` -${data.firstName}` + " " + data.lastName;
         })
-        .catch(error => {
-            console.error('Error while posting the quote:', error);
-        });
+
+    //     try {
+           
+    //     } catch (error) {
+    //         const imgElement = document.getElementById("myImg")! as HTMLImageElement;
+    //         if (imgElement.src = "https://i.pinimg.com/564x/81/5b/55/815b55146b9ff6555986bad7b9bc00d6.jpg") {
+    //             imgElement.src = "https://i.pinimg.com/736x/6c/f6/62/6cf662ab5632f2df41e84e5d5144d2d9.jpg"
+    //             document.getElementById("quoteDisplay")!.innerText = "I just have to win. . . right, Izuku?"
+    //         }
+    //         else {
+    //             imgElement.src = "https://i.pinimg.com/564x/81/5b/55/815b55146b9ff6555986bad7b9bc00d6.jpg"
+    //             document.getElementById("quoteDisplay")!.innerText = "I'll never go back on my Word. That's my Nindo: my ninja way!!";
+    //         }
+    //     }
+    // }
+
+    async function PostQuote(newQuote: { firstName: string, lastName: string, _Quote: string, image: string }) {
+        await fetch('https://localhost:7028/api/Quote', {
+            method: 'POST', // Specify the HTTP method as POST
+            headers: {
+                'Content-Type': 'application/json' // Set the content type to JSON
+            },
+            body: JSON.stringify(newQuote) // Convert the JavaScript object to a JSON string
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); // Parse the JSON from the response
+            })
+            .then(data => {
+                console.log('Quote successfully posted:', data);
+                // You can handle the response data if needed
+            })
+            .catch(error => {
+                console.error('Error while posting the quote:', error);
+            });
+    }
 }
