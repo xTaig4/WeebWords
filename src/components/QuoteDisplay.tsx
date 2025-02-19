f8952c5312beca1fbcaab3bf936e6521f1ff20f5
TaiStudent <73821893+xTaig4@users.noreply.github.com>
2025-02-19 11:58:11 +0100

    random button now gets random quote from DB (RandomQuoteFromDb())

--- src/components/QuoteDisplay.tsx
+++ src/components/QuoteDisplay.tsx
@@ -9,14 +9,14 @@ const QuoteDisplay: React.FC = () => {
                             Quote text for display  <br></br>
                             I just have to win. . . right, Izuku?
                         </pre>
-                        <button className='random-button' onClick={GetRandomQuote}> Random quote! </button>
+                        <button className='random-button' onClick={RandomQuoteFromDb}> Random quote! </button>
                     </div>
 
                     <div className='quote-right-column'>
                         <div className='quote-img-Container'>
                             <img id='myImg'
                                 src='https://i.pinimg.com/736x/6c/f6/62/6cf662ab5632f2df41e84e5d5144d2d9.jpg'
-                                alt="alt text"
+                                alt="Quote image"
                             ></img>
                         </div>
 
@@ -39,4 +39,21 @@ async function GetRandomQuote() {
             document.getElementById("quote")!.innerText = data._Quote + ` -${data.firstName}` + " " + data.lastName;
         })
 }
+
+async function RandomQuoteFromDb() {
+    await fetch('http://localhost:5000/api/quotes/random')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Parse the JSON from the response 
+        } )
+        .then(data => {
+            const imgElement = document.getElementById("myImg")! as HTMLImageElement;
+            imgElement.src = data.image_url;
+            document.getElementById("quote")!.innerText = data.quote + ` -${data.firstName}` + " " + data.lastName;
+        }
+    )
+}
+
 export default QuoteDisplay;
